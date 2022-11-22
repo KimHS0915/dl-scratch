0dfd0cd5197df147c8082713e47b07a04ffbc42e
KimHS0915 <remember2552@gmail.com>
2022-11-22 22:27:47 +0900

    Add Nesterov, RMSprop

--- my_lib/optimizer.py
+++ my_lib/optimizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class SGD:
     def __init__(self, lr=0.01):
         self.lr = lr
@@ -7,6 +8,7 @@ class SGD:
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
 
 class Momentum:
     def __init__(self, lr=0.01, momentum=0.9):
@@ -24,6 +26,25 @@ class Momentum:
             self.v[key] = (self.momentum * self.v[key]) - (self.lr * grads[key])
             params[key] += self.v[key]
 
+
+class Nesterov:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+            params[key] += self.momentum * self.momentum * self.v[key]
+            params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
+
 class AdaGrad:
     def __init__(self, lr=0.01):
         self.lr = lr
@@ -38,6 +59,7 @@ class AdaGrad:
         for key in params.keys():
             self.h[key] += grads[key] * grads[key]
             params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key] + 1e-7))
+
 
 class Adam:
     def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
@@ -63,3 +85,19 @@ class Adam:
             self.v[key] += (1 - self.beta2) * (grads[key] ** 2 - self.v[key])
             params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
 
+
+class RMSprop:
+    def __init__(self, lr=0.01, decay_rate=0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+    
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
